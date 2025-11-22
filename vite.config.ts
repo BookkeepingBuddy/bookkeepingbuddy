9c220a02a0b366017ea1aabf0ac3b54210652089
Berend-Jan Lange <berendjanlange@Mac.home>
2025-11-22 01:51:02 +0100

    update base url

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/bookkeepingbuddy/",
+  base: mode === "production" ? "/bookkeepingbuddy/" : "/",
   server: {
     host: "::",
     port: 8080,
